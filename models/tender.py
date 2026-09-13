5a888425632df431d4645f04cbebf45005cff7cb
sammzy-cloud <samanthangiela@gmail.com>
2026-09-13 15:14:48 +0300

    Created a function that allows new tenders to be added

--- models/tender.py
+++ models/tender.py
@@ -1,1 +1,22 @@
-   
+class Tender:
+    """A single tender entry."""
+
+    _next_id = 1  
+    STATUSES = ("open", "closed", "awarded")
+
+    def __init__(self, title, description, deadline, budget, created_by,
+                 status="open", tender_id=None):
+        if tender_id is None:
+            tender_id = Tender._next_id
+            Tender._next_id += 1
+        else:
+            Tender._next_id = max(Tender._next_id, tender_id + 1)
+
+        self._id = tender_id
+        self._title = title
+        self._description = description
+        self._deadline = deadline
+        self._budget = budget
+        self._created_by = created_by
+        self.status = status  
+
