55bb69cf75c60aeb31468cb6c730fa67ba8cd680
daniellesiamon-cpu <daniel.lesiamon@student.moringaschool.com>
2026-09-13 03:25:17 -0600

    Complete CLI menu rendering and main run loop in main.py

--- main.py
+++ main.py
@@ -119,3 +119,4 @@ class App:
             tender_id= Prompt.ask("Tender ID to award")
             tender_item = self.tenders.find_by_id(_safe_int(tender_id))
             if tender_item is None:
+                 console.print("[red]Tender not found.[/red]")
